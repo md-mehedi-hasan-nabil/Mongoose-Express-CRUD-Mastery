7da765804800477099a9a4fac87a2da2d2471164
Md. Mehedi Hasan Nabil <mehedihasannabil49@gmail.com>
2023-11-23 20:59:48 +0600

    project setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
-import cors from "cors"
+import cors from "cors";
+import morgan from "morgan"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('tiny'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
--- src/server.ts
+++ src/server.ts
@@ -8,8 +8,10 @@ async function main() {
     try {
         await mongoose.connect(config.db_uri as string);
 
+        console.log("Database connection successful")
+
         app.listen(config.port, () => {
-            console.log(`App listening on port ${config.port}`)
+            console.log(`Server is listening on port ${config.port}`)
         })
     } catch (error) {
         console.error(error)
